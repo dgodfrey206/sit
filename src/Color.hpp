38d5258f4403b780d34db817ff5763c48d28072e
David Godfrey <david.godfrey99@gmail.com>
2022-06-04 22:36:01 +0800

    color support for windows. rewrote Makefile

--- src/Color.hpp
+++ src/Color.hpp
@@ -2,6 +2,11 @@
 #define _SRC_COLOR_HPP
 #include <string>
 
+#ifdef _WIN32
+	#include <windows.h>  // for WinAPI and Sleep()
+	#define _NO_OLDNAMES  // for MinGW compatibility
+#endif
+
 namespace Sit {
 namespace Color {
 
@@ -44,6 +49,51 @@ const std::string LIGHTMAGENTA = "\033[01;35m"; // Light magenta / light purple
 const std::string LIGHTCYAN = "\033[01;36m";    // Light cyan
 const std::string WHITE = "\033[01;37m";        // White (bright)
 #endif
+
+class ColorType {
+#ifdef _WIN32
+	int windows;
+#else
+	std::string ansi;
+#endif
+public:
+	friend std::ostream& operator<<(std::ostream &out, const ColorType& c);
+#ifdef _WIN32
+	ColorType(const std::string &a, const int w): windows(w) {}
+#else
+	ColorType(const std::string &a, const int w): ansi(a) {}
+#endif
+};
+
+inline std::ostream& operator<<(std::ostream &out, const ColorType& c) {
+#ifdef _WIN32
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, (WORD)c.windows);
+#else
+	out << c.ansi;
+#endif
+	return out;
+}
+
+extern const ColorType RESET       ;
+extern const ColorType CLS         ; // Clears screen
+extern const ColorType BLACK       ; // Black
+extern const ColorType RED         ; // Red
+extern const ColorType GREEN       ; // Green
+extern const ColorType BROWN       ; // Brown / dark yellow
+extern const ColorType BLUE        ; // Blue
+extern const ColorType MAGENTA     ; // Magenta / purple
+extern const ColorType CYAN        ; // Cyan
+extern const ColorType GREY        ; // Grey / dark white
+extern const ColorType DARKGREY    ; // Dark grey / light black
+extern const ColorType LIGHTRED    ; // Light red
+extern const ColorType LIGHTGREEN  ; // Light green
+extern const ColorType YELLOW      ; // Yellow (bright)
+extern const ColorType LIGHTBLUE   ; // Light blue
+extern const ColorType LIGHTMAGENTA; // Light magenta / light purple
+extern const ColorType LIGHTCYAN   ; // Light cyan
+extern const ColorType WHITE       ; // White (bright)
+
 }
 }
 
