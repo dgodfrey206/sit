8a688c7b8e61da0b7a1b0f80fcfa05e9fe0e9d5a
David Godfrey <david.godfrey99@gmail.com>
2022-06-04 22:36:01 +0800

    fix a bug when remove files from index fix a bug when print Args add help messages => sit help or sit --help

--- src/Core.cc
+++ src/Core.cc
@@ -97,7 +97,7 @@ void Add(const boost::filesystem::path &path)
 
 void Rm(const boost::filesystem::path &path)
 {
-	std::cout << "Removed " << Index::index.Remove(path) << " files" << std::endl;
+	Index::index.Remove(FileSystem::GetRelativePath(path));
 	Index::index.Save();
 }
 
